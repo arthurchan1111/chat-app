6eae5f2bf0c80d6b1cdeaa6fb1ca12abb015f537
Arthur Chan <arthurchan1111@gmail.com>
2017-12-27 19:21:52 -0500

    Feat: Add socket io logic

--- src/components/chat_input.tsx
+++ src/components/chat_input.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
+import * as socketio  from "socket.io-client";
 
 export class Input extends React.Component{
+
+
+  private socket:any = socketio.connect();
+
 send(){
   alert("send");
 }
 render() {
-  return (<div><form><input type="text" placeholder="Enter Message Here"/><button onClick={()=>this.send()} type="submit">Send </button></form>
-
-  </div>);
+  return (
+    <form onSubmit = {() => this.send()}>
+    <input type="text" placeholder="Enter Message Here"/>
+    <button type="submit">Send </button>
+    </form>
+    );
 }
 
 
