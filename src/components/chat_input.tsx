ff41b42165d53acb67ad622da0514a16d6eba73f
Arthur Chan <arthurchan1111@gmail.com>
2017-12-28 22:15:32 -0500

    Feat: Update send function for input

--- src/components/chat_input.tsx
+++ src/components/chat_input.tsx
@@ -1,18 +1,25 @@
 import * as React from "react";
 import * as socketio  from "socket.io-client";
 
-export class Input extends React.Component{
+export class Input extends React.Component<{}, { value: string }>{
+private socket:any = socketio.connect();
+  constructor(props:any){
+    super(props);
+    this.state = {value: ''};
+     this.send = this.send.bind(this);
 
-
-  private socket:any = socketio.connect();
+  }
 
 send(){
-  alert("send");
+
+  this.socket.emit('chat message', this.state.value);
+  this.state = {value: ''};
+
 }
 render() {
   return (
     <form onSubmit = {() => this.send()}>
-    <input type="text" placeholder="Enter Message Here"/>
+    <input type="text" placeholder="Enter Message Here" value={this.state.value}/>
     <button type="submit">Send </button>
     </form>
     );
